1591d86fb038223ff191df5aa01dc686fcec4dcc
gunny091 <ryuee091119@naver.com>
2022-03-08 20:33:04 +0900

    add stopwatch

--- src/clock.js
+++ src/clock.js
@@ -1,14 +1,14 @@
 const clock = document.querySelector("h1#clock");
 
 function showTimeToClock() {
-  const now = new Date();
+    const now = new Date();
 
-  const hour = String(now.getHours()).padStart(2, "0");
-  const min = String(now.getMinutes()).padStart(2, "0");
-  const sec = String(now.getSeconds()).padStart(2, "0");
+    const hour = String(now.getHours()).padStart(2, "0");
+    const min = String(now.getMinutes()).padStart(2, "0");
+    const sec = String(now.getSeconds()).padStart(2, "0");
 
-  clock.innerText = `${hour}:${min}:${sec}`;
+    clock.innerText = `${hour}:${min}:${sec}`;
 }
 
 showTimeToClock();
-setInterval(showTimeToClock, 100);
+setInterval(showTimeToClock, 1000);
--- src/stopwatch.js
+++ src/stopwatch.js
@@ -3,10 +3,99 @@ const startButton = document.querySelector("input#start");
 const resetButton = document.querySelector("input#reset");
 
 let startTime = 0;
-let started = false;
+let startTimeAddPause = 0;
+let endTime = 0;
+let isRunning = false;
+let stopped = false;
+let displayTrigger = false;
+
+const CLICKED = "clicked";
 
 function start() {
-    startTime = new Date().getTime();
-    started = true;
+    if (!stopped) {
+        now = new Date().getTime();
+        startTime = now;
+        startTimeAddPause = now;
+    } else {
+        stopped = false;
+        now = new Date().getTime();
+        startTimeAddPause += now - endTime;
+    }
+    isRunning = true;
+    startButton.value = "정지";
 }
-start();
+
+function stop() {
+    endTime = new Date().getTime();
+    isRunning = false;
+    stopped = true;
+    displayTrigger = true;
+    startButton.value = "다시 시작";
+}
+
+function makeText(time) {
+    const hour = String(Math.floor(time / 3600000) % 24).padStart(2, "0");
+    const min = String(Math.floor(time / 60000) % 60).padStart(2, "0");
+    const sec = String(Math.floor(time / 1000) % 60).padStart(2, "0");
+    const msec = String(Math.floor(time / 10) % 100).padStart(2, "0");
+
+    return `${hour}:${min}:${sec}.${msec}`;
+}
+
+function showTimeToStopwatch() {
+    let text;
+    if (isRunning) {
+        text = makeText(new Date().getTime() - startTimeAddPause);
+    } else if (displayTrigger) {
+        if (stopped) {
+            text = makeText(endTime - startTimeAddPause);
+        } else {
+            text = "00:00:00.00";
+        }
+    }
+    if (isRunning || displayTrigger) {
+        displayTrigger = false;
+        stopwatch.innerText = text;
+    }
+}
+
+function reset() {
+    startTime = 0;
+    startTimeAddPause = 0;
+    endTime = 0;
+    isRunning = false;
+    stopped = false;
+    displayTrigger = true;
+    startButton.value = "시작";
+}
+
+function startButtonHandler() {
+    if (isRunning) {
+        stop();
+    } else {
+        start();
+    }
+}
+function resetButtonHandler() {
+    reset();
+}
+
+startButton.addEventListener("click", startButtonHandler);
+resetButton.addEventListener("click", resetButtonHandler);
+
+window.addEventListener("keydown", e => {
+    if (e.key === " ") {
+        startButtonHandler();
+        startButton.classList.add(CLICKED);
+        setTimeout(() => {
+            startButton.classList.remove(CLICKED);
+        }, 100);
+    } else if (e.key === "Shift") {
+        resetButtonHandler();
+        resetButton.classList.add(CLICKED);
+        setTimeout(() => {
+            resetButton.classList.remove(CLICKED);
+        }, 100);
+    }
+});
+setInterval(showTimeToStopwatch, 10);
